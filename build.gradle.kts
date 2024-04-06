fa57b1acfe2fa60f35baf93fe674f146791dc1e3
alyumi <lanton.1999@mail.ru>
2024-04-06 23:37:51 +0500

    Add parametrized tests, allure

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,17 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+val allureVersion = "2.25.0"
+
+// Define the version of AspectJ
+val aspectJVersion = "1.9.21"
+
+// Define configuration for AspectJ agent
+val agent: Configuration by configurations.creating {
+    isCanBeConsumed = true
+    isCanBeResolved = true
+}
+
 repositories {
     mavenCentral()
 }
@@ -19,8 +30,21 @@ dependencies {
     testImplementation("io.rest-assured:rest-assured:5.4.0")
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
     testImplementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+
+    // Import allure-bom to ensure correct versions of all the dependencies are used
+    testImplementation(platform("io.qameta.allure:allure-bom:$allureVersion"))
+    // Add necessary Allure dependencies to dependencies section
+    testImplementation("io.qameta.allure:allure-rest-assured")
+    testImplementation("io.qameta.allure:allure-junit5")
+
+    // Add aspectjweaver dependency
+    agent("org.aspectj:aspectjweaver:${aspectJVersion}")
 }
 
 tasks.test {
     useJUnitPlatform()
+
+    jvmArgs = listOf(
+            "-javaagent:${agent.singleFile}"
+    )
 }
